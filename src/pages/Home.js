0bfa0ad466f61a6a77729857b5b73307ae47da0b
Aly Baez <alybaez@Alys-MBP.lan>
2021-12-28 20:33:47 -0800

    adds styling to bottom modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import DailyInfoCard from "../components/DailyInfoCard";
 import Header from "../components/Header";
+import RoomInfoCard from "../components/RoomInfoCard";
 import style from "../style/home.module.css";
+import AppsIcon from "@mui/icons-material/Apps";
+import AddIcon from "@mui/icons-material/Add";
+import data from "../data/roomCard.json";
+import BottomSheet from "../components/BottomSheet";
 
 function Home() {
+  const [itemsVisible, setItemsVisible] = useState(true);
+  const [sheetVisible, setSheetVisible] = useState(false);
+  const [sheetCreateRoom, setSheetCreateRoom] = useState(false);
+  const [loaderVisibility, setLoaderVisibility] = useState(false);
+  const [cardId, setCardId] = useState(1);
+
   return (
     <>
       <Header />
       <div className={style.home_container}>
         <DailyInfoCard />
+        <RoomInfoCard />
       </div>
+      <div className={style.action_btn}>
+        <button onClick={() => setSheetVisible(true)}>
+          <AddIcon className="mr-2" />
+          Start a Room
+        </button>
+        <button>
+          <AppsIcon />
+        </button>
+      </div>
+      <BottomSheet
+        sheetTitle="start room"
+        setSheetVisible={(item) => setSheetVisible(item)}
+        sheetVisible={sheetVisible}
+        cardDetail={data.find((item) => item.id == cardId)}
+        setItemsVisible={(item) => setItemsVisible(item)}
+        setSheetCreateRoom={(item) => {
+          setLoaderVisibility(true);
+          setTimeout(() => {
+            setSheetCreateRoom(item);
+            setLoaderVisibility(false);
+          }, 1000);
+        }}
+      />
     </>
   );
 }
